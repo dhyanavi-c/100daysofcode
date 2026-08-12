50775f24901e3ffc4cf63ff56ef679ac4c478021
dhyanavi chhunchha <dhyanavi.chhunchha@gmail.com>
2026-08-12 20:43:13 +0530

    Day 3: Solve Q5 and Q6 (User Inputs, Operations & Output)

---
+++ day3q2.c
@@ -0,0 +1,20 @@
+// Write a program to swap two numbers using a third variable.
+
+#include <stdio.h>
+
+	int main(){
+	int a,b,c;
+
+	printf("enter two numbers:");
+	scanf("%d%d",&a,&b);
+
+	c=a;
+	a=b;
+	b=c;
+
+	printf("after swapping:");
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+
+	return 0;
+}
